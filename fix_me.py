b40759fe2900cc25ee7e3e9e8cf817f914963a92
Audie <augh.breslin@gmail.com>
2021-09-15 22:06:58 -0400

    the only correct answer

--- fix_me.py
+++ fix_me.py
@@ -1,3 +1,3 @@
 # 2 + 2 = ?
 # Print the answer right below this line!
-print("Vendawg Kevin bad, marco + rocco = morocco")
+print("Vendawg Kevin bad, marco + rocco = country to the left of algeria")
